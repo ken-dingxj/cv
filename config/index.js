fd57bf671c3d354dcf99e83519aff9470e7ded8d
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-04-27 18:45:00 +0800

    feat(init): 初始化界面

---
+++ config/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  dev: {
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    host: 'localhost',
+    port: 8080,
+    autoOpenBrowser: true,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false,
+    mode: 'development',
+    /**
+     * Source Maps
+     */
+    devtool: 'cheap-module-eval-source-map',
+    /**
+     *
+     * 浏览器缓存相关
+     *
+     */
+    cacheBusting: true,
+    /**
+     *
+     * css源文件映射
+    *
+     */
+    cssSourceMap: true,
+  },
+  build: {
+    mode: 'production',
+    devtool: '#source-map',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../dist'),
+  },
+};
